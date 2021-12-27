38714322ff9eaeb88ddcb7b5a60b7ef198f65a06
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2021-12-27 14:20:46 +0100

    add methodes to orders service

--- NetStoreClient/src/app/shared/services/orders.service.ts
+++ NetStoreClient/src/app/shared/services/orders.service.ts
@@ -19,4 +19,45 @@ export class OrdersService {
             catchError((error: any) => throwError(error))
         );
   }
+
+  public getCompletedOrders() {
+    return this.client.completedOrders()
+        .pipe(
+            map((data) => data),
+            catchError((error: any) => throwError(error))
+        );
+  }
+
+  public getOrderById(id: string) {
+    return this.client.getOrder(id)
+        .pipe(
+            map((data) => data),
+            catchError((error: any) => throwError(error))
+        );
+  }
+
+  public getOrderofUser(userId: string) {
+    return this.client.orderByUser(userId)
+        .pipe(
+            map((data) => data),
+            catchError((error: any) => throwError(error))
+        );
+  }
+
+
+  public countOrders() {
+    return this.client.countOrders()
+        .pipe(
+            map((data) => data),
+            catchError((error: any) => throwError(error))
+        );
+  }
+
+  public markOrderCompleted(orderId: string) {
+    return this.client.markOrderCompleted(orderId)
+        .pipe(
+            map((data) => data),
+            catchError((error: any) => throwError(error))
+        );
+  }
 }
